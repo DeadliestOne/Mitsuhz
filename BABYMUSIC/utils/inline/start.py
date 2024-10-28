08e66680acc7f2d86776ce3d2092b9d045320435
DeadliestOne <uncountableaura@gmail.com>
2024-10-28 14:43:16 +0530

    Update start.py

--- BABYMUSIC/utils/inline/start.py
+++ BABYMUSIC/utils/inline/start.py
@@ -1,16 +1,14 @@
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, Message
+from pyrogram.types import InlineKeyboardButton
+
 import config
-import asyncio
-from BABYMUSIC import app
+from AnonXMusic import app
 
 
-# Start panel for inline buttons
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["SO_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
@@ -18,7 +16,6 @@ def start_panel(_):
     return buttons
 
 
-# Private panel for inline buttons
 def private_panel(_):
     buttons = [
         [
@@ -29,12 +26,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], url=config.CHAT),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.SOURCE),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
